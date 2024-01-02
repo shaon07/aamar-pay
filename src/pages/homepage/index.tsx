7ea91e307ec3c1be8c2c29bfd071f8ac354dc261
Shaon07 <alishaon77@gmail.com>
2024-01-02 23:09:31 +0600

    [add] initial redux toolkit setup

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,8 +1,29 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from "../../services/redux/slices/counterSlice"
+import { RootState } from "../../services/redux/store"
 
 export default function Homepage() {
+    const count = useSelector((state: RootState) => state.counterReducer.value)
+    const dispatch = useDispatch()
+
     return (
         <div>
             <h1>Homepage</h1>
+            <div>
+                <button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+            </div>
         </div>
     )
 }
